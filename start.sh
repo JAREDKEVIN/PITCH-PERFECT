7a8dc4ba1da081246058036889fb68b0e4e7b2ae
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-05-17 12:55:03 +0300

    set username and password

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export MAIL_USERNAME='evesambu57@gmail.com'
-export MAIL_PASSWORD='titravic'
+export MAIL_USERNAME='kevinkipkemoi'
+export MAIL_PASSWORD='pitchperfect2022'
 SECRETE_KEY='no474tghff8735757t5hf75t77t8'
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:kimachas@localhost/pitch'
 python3 manage.py server
